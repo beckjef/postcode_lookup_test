4ac96c8052eb4a4566a4b31d537eda0e3750f7e7
Jennifer Bufton <jennifer.bufton@sportengland.org>
2021-04-07 14:51:15 +0100

    Added reference to AP

--- postcode.py
+++ postcode.py
@@ -66,6 +66,13 @@ try:
         df['IMD Rank'].iloc[i] = s.json()['features'][0]['attributes']['IMDRank0']
         df['IMD Score'].iloc[i] = s.json()['features'][0]['attributes']['IMDScore']
 
+    # append AP
+    ap_df = pd.read_csv('https://raw.githubusercontent.com/jenniferbufton/Postcode-lookup/main/LA_ActivePartnerships_lookup_20200512.csv')
+    ap_df = ap_df[['LA_Name', 'Active_Partnership_Label']]
+    df = df.merge(ap_df, how='inner', left_on= 'Local Authority', right_on='LA_Name')
+    df.drop(['LA_Name'], axis=1, inplace=True)
+    df.rename({'Active_Partnership_Label': 'Active Partnership'}, axis=1, inplace=True)
+
     try:
     # create map
         m = folium.Map(location=[df['Latitude'][0], df['Longitude'][0]],
@@ -88,11 +95,8 @@ try:
 
     style_0 = {'fillColor': '#2ca25f',  'color': '#2ca25f', "fillOpacity": 0.1, "weight": 1.7}
 
-    #geo_ap =  folium.GeoJson(data=(open("AP.json", "r", encoding="utf-8-sig")).read()).add_to(m)
-
     ap = requests.get('https://raw.githubusercontent.com/jenniferbufton/flood_app/main/data/AP.json').json()
 
-    #ap_json = folium.GeoJson(data=('https://raw.githubusercontent.com/jenniferbufton/flood_app/main/AP.json'), style_function = lambda x:style_0).add_to(m)
     fg = folium.FeatureGroup(name='Active Partnership', show=True)
     m.add_child(fg)
 
